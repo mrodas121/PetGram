74eeb481d3373120472b3f87293c8b5065dc48ef
mrodas121 <manuel.rodas121@gmail.com>
2022-01-13 22:38:01 -0600

    agregando informacion real a list of categortys+

--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import { Anchor, Image } from './styles'
 
-const DEFAULT_IMAGE = 'https://i.imgur.com/dJa0Hpl.jpg'
-
-export const Category = ({ cover = DEFAULT_IMAGE, path, emoji = '?' }) => (
-  <Anchor href={path}>
-    <Image src={cover} />
-    {emoji}
+export const Category = ({ Category }) => (
+  <Anchor href={Category.path}>
+    <Image src={Category.cover} />
+    {Category.emoji}
   </Anchor>
 )
--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Category } from '../Category'
 import {List, Item} from './styles'
+import db from '../../../api/db.json'
 
 export const ListOfCategories = () => {
     return(
         <List>
             {
-                [1,2,3,4].map(cateogory=> <Item key={Category}><Category/></Item>)
+                db.categories.map(category=> <Item key={category.id}><Category Category= {category}/></Item>)
             }
         </List>
     )
