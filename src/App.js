def9e11b55fca38cb0aeb1c8953a859f9c140f5b
mrodas121 <manuel.rodas121@gmail.com>
2022-01-13 22:53:02 -0600

    componentes de photocard y list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
+import { ListOfPhotoCards } from './components/ListOfPhotoCard/'
 import { GlobalStyle } from './GlobalStyles'
 const App = () => {
     return(
         <>
             <GlobalStyle />
-            <ListOfCategories/>        
+            <ListOfCategories/>    
+            <ListOfPhotoCards />    
         </>
     )
 }
