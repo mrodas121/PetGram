ba331a556350f15e1cde22b2ef429bcb7e34f2e8
mrodas121 <manuel.rodas121@gmail.com>
2022-01-14 22:11:22 -0600

    cambios para uso de logo, animaciones y ordenamiento de estilos globales

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
 import { ListOfPhotoCards } from './components/ListOfPhotoCard/'
-import { GlobalStyle } from './GlobalStyles'
+import { GlobalStyle } from './styles/GlobalStyles'
+import { Logo } from './components/Logo'
 const App = () => {
     return(
         <>
+            <Logo />
             <GlobalStyle />
             <ListOfCategories/>    
             <ListOfPhotoCards />    
