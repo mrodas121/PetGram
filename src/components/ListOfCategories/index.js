75ba130933105aebaf179c39c33c8a9e50b98c6a
mrodas121 <manuel.rodas121@gmail.com>
2022-01-15 22:57:15 -0600

    cambios para useeffect

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
 import { Category } from '../Category'
-import {List, Item} from './styles'
+import { List, Item } from './styles'
 import db from '../../../api/db.json'
+import { render } from 'react-dom'
 
-export const ListOfCategories = () => {
-    return(
-        <List>
+export const ListOfCategories = (fixed) => {
+    const [ categories, setCategories ] = useState(db.categories)
+
+    useEffect(function () {
+        const onScroll = e => {
+          const newShowFixed = window.scrollY > 200
+          showFixed !== newShowFixed && setShowFixed(newShowFixed)
+        }
+    
+        document.addEventListener('scroll', onScroll)
+    
+        return () => document.removeEventListener('scroll', onScroll)
+      }, [showFixed])
+    const [showFixed, setShowFixed] = useState(false)
+    const renderList = (fixed) => (
+        <List className={fixed ? 'fixed' : ''}>
             {
-                db.categories.map(category=> <Item key={category.id}><Category Category= {category}/></Item>)
+                categories.map(category=> <Item key={category.id}><Category Category= {category}/></Item>)
             }
         </List>
+    )
+    return(
+        <Fragment>
+            {renderList()}
+            {showFixed && renderList(true)}
+        </Fragment>
     )
 }
