1174a29782538584d6bc86ac8762847512d16da3
mrodas121 <manuel.rodas121@gmail.com>
2022-01-22 21:39:32 -0600

    cambios para navbar

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
 import React from 'react'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
-import {PhotoCardWithQuery} from './container/PhotoCardWithQuery'
 import Home from './pages/home'
+import { Detail } from './pages/Detail'
 import { Router } from '@reach/router'
+import { NavBar } from './components/NavBar'
 const App = () => {
-    const urlParams = new window.URLSearchParams(window.location.search)
-    const detailId = urlParams.get('detail')
-    console.log(detailId)
     return (
         <>
             <GlobalStyle />
             <Logo />
-            {
-                detailId
-                ?
-                    <PhotoCardWithQuery id={detailId} />
-                :
-                <Router>
-                     <Home path='/'/>
-                     <Home path='/pet/:categoryId'/>
-                </Router>
-            }
+            <Router>
+                 <Home path='/'/>
+                 <Home path='/pet/:categoryId'/>
+                 <Detail path='/detail/:detailId' />
+            </Router>
+            <NavBar/>
         </>)
 }
 export default App
